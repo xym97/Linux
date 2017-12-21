0777869e94d26db7b2a0885391840cec9dfdec0d
root <root@localhost.localdomain>
2017-12-20 19:27:00 -0800

    模拟shell

--- process/myshell.c
+++ process/myshell.c
@@ -1,66 +1,68 @@
+#include<stdio.h>
 #include<stdlib.h>
 #include<sys/types.h>
 #include<sys/wait.h>
 #include<unistd.h>
 #include<stdio.h>
-
-int main()
-{
-//    pid_t id = fork();
-//    if(id == 0){
-//        printf("execl running \n");
-//        sleep(1);
-//        //execl("/bin/ls","ls","-a","-l","-n",NULL);
-//        //execlp("ls","ls","-a","-l","-n",NULL);
-//        char* argv[] = {
-//            "ls", "-a","-l","-n",NULL
-//        };
-//        //execl("/bin/ls","ls","-a","-i","-n",NULL);
-//        //execv("/bin/ls",argv);
-//        //execvp("ls",argv);
-//       // execlp("ls","ls","-l","-i",NULL);
-//        char* _env[] = {"MYENV=/FORTEST/A/B/C/D/E",NULL,};    
-//        execle("./mybin","mybin",NULL,_env);
-//        exit(1);
-//    }else{
-//       pid_t ret =  waitpid(id,NULL,0);
-//       if(ret > 0){
-//            printf("waitpid success\n");
-//       }
-//    }
+#include<sys/stat.h>
+#include<sys/fcntl.h>
+#include<string.h>
+#include<ctype.h>
+ 
+int main(){
     while(1){
         printf("[ym@localhost process myshell]$ ");
-        fflush(stdout);
-        char buf[1024];
+        fflush(stdout);//刷新输出缓冲区  打印命令行解释器的头部信息
+        char buf[1024];//定义一个缓冲区  用来存储从键盘输入的命令
         ssize_t s = read(0,buf,sizeof(buf)-1);
         if(s > 0){
-            buf[s-1] = 0;
-            printf("%s\n", buf);
+            buf[s-1] = 0;//确保缓冲区最后的‘\0’
+            //printf("%s\n", buf);
         }
         char* start = buf;
-        char* _argv[32];
-        _argv[0] = start;
-        int i = 1;
+        char* _argv[32] = {NULL};
+        int i = 0;//将存储命了的缓冲区中的命令参数存入_argv[]  空格填上 ‘\0’
         while(*start){
-            if(isspace(*start)){
+            while(*start && isspace(*start)){
                 *start = 0;
                 start++;
-                _argv[i++] = start;
-            }else{
+            }
+            _argv[i++] = start;
+            while(*start && !isspace(*start)){
                 start++;
             }
         }
-        _argv[i] = NULL;
-        pid_t id = fork();
+        _argv[i] = NULL;//_argv[] 要作为execv的参数 所以末尾必须加NULL
+        pid_t id = fork();//依照shell的原理创建子进程
         if(id == 0){
-            execvp(_argv[0],_argv);
-            exit(1);
-        }else{
-            pid_t ret = waitpid(id, NULL, 0);
-            if(ret > 0){
-                printf("wait child success!\n");
+            int flag = 0;
+            int i = 0;
+            for( ; _argv[i] != NULL; ++i ){//寻找 命令行参数中的 ‘>’ 重定向符号
+                if(strcmp(">", _argv[i]) == 0){
+                    flag = 1;//flag设置为1 表示有重定向符号
+                    break;
+                }
             }
-        }
-    }
-    return 0;
+            int copyfd;
+            _argv[i] = NULL;//要作为execv的参数 _argv[i]要设置为 NULL
+            if(flag){
+                close(1);//关闭标准输出缓冲区
+                int fd = open(_argv[i + 1], O_RDWR | O_CREAT, 0755); //此时fd为1
+               //copyfd = dup2(1, fd); 
+           }
+           execvp(_argv[0],_argv);//被替换的程序 如果有重定向 因为PCB没有变 file结构体没有变  文件描述符没有变
+           //此时原本默认的输出到文件描述符为1的文件 已经不是显示屏  而是新打开的一个文件
+           //if(flag){
+           //   close(1);
+           //   dup2(copyfd, 1);
+           //}
+           exit(1);//进程退出时会对自己打开的文件描述符关闭   下一次while（1）的循环开始时 重新创建子进程 默认打开 0 1 2 stdin stdout stderror
+       }else{
+           pid_t ret = waitpid(id, NULL, 0);
+           if(ret > 0){
+              // printf("wait child success!\n");
+           }
+       }
+   }
+   return 0;
 }
